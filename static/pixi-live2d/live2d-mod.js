575b11e9c0593baabf4cecaef33a1d483b104a97
huangup <594204332@qq.com>
2020-03-10 16:36:00 +0800

    '修改live2d使用路径'

--- static/pixi-live2d/live2d-mod.js
+++ static/pixi-live2d/live2d-mod.js
@@ -17,38 +17,38 @@ renderer.view.style.zIndex = 1000;
 const modelHaru = {
 	"type": "Live2D Model Setting",
 	"name": "haru",
-	"model": "pixi-live2d/assets/haru/haru.moc",
+	"model": "static/pixi-live2d/assets/haru/haru.moc",
 	"textures": [
-		"pixi-live2d/assets/haru/haru.1024/texture_00.png",
-		"pixi-live2d/assets/haru/haru.1024/texture_01.png",
-		"pixi-live2d/assets/haru/haru.1024/texture_02.png"
+		"static/pixi-live2d/assets/haru/haru.1024/texture_00.png",
+		"static/pixi-live2d/assets/haru/haru.1024/texture_01.png",
+		"static/pixi-live2d/assets/haru/haru.1024/texture_02.png"
 	],
-	"physics": "pixi-live2d/assets/haru/haru.physics.json",
-	"pose": "pixi-live2d/assets/haru/haru.pose.json",
+	"physics": "static/pixi-live2d/assets/haru/haru.physics.json",
+	"pose": "static/pixi-live2d/assets/haru/haru.pose.json",
 	"expressions": [{
 		"name": "f01",
-		"file": "pixi-live2d/assets/haru/expressions/f01.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f01.exp.json"
 	}, {
 		"name": "f02",
-		"file": "pixi-live2d/assets/haru/expressions/f02.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f02.exp.json"
 	}, {
 		"name": "f03",
-		"file": "pixi-live2d/assets/haru/expressions/f03.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f03.exp.json"
 	}, {
 		"name": "f04",
-		"file": "pixi-live2d/assets/haru/expressions/f04.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f04.exp.json"
 	}, {
 		"name": "f05",
-		"file": "pixi-live2d/assets/haru/expressions/f05.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f05.exp.json"
 	}, {
 		"name": "f06",
-		"file": "pixi-live2d/assets/haru/expressions/f06.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f06.exp.json"
 	}, {
 		"name": "f07",
-		"file": "pixi-live2d/assets/haru/expressions/f07.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f07.exp.json"
 	}, {
 		"name": "f08",
-		"file": "pixi-live2d/assets/haru/expressions/f08.exp.json"
+		"file": "static/pixi-live2d/assets/haru/expressions/f08.exp.json"
 	}],
 	"layout": {
 		"center_x": 0,
@@ -64,44 +64,44 @@ const modelHaru = {
 	}],
 	"motions": {
 		"idle": [{
-			"file": "pixi-live2d/assets/haru/motions/idle_00.mtn",
+			"file": "static/pixi-live2d/assets/haru/motions/idle_00.mtn",
 			"fade_in": 2000,
 			"fade_out": 2000
 		}, {
-			"file": "pixi-live2d/assets/haru/motions/idle_01.mtn",
+			"file": "static/pixi-live2d/assets/haru/motions/idle_01.mtn",
 			"fade_in": 2000,
 			"fade_out": 2000
 		}, {
-			"file": "pixi-live2d/assets/haru/motions/idle_02.mtn",
+			"file": "static/pixi-live2d/assets/haru/motions/idle_02.mtn",
 			"fade_in": 2000,
 			"fade_out": 2000
 		}],
 		"tap_body": [{
-			"file": "pixi-live2d/assets/haru/motions/tapBody_00.mtn",
-			"sound": "pixi-live2d/assets/haru/sounds/tapBody_00.mp3"
+			"file": "static/pixi-live2d/assets/haru/motions/tapBody_00.mtn",
+			"sound": "static/pixi-live2d/assets/haru/sounds/tapBody_00.mp3"
 		}, {
-			"file": "pixi-live2d/assets/haru/motions/tapBody_01.mtn",
-			"sound": "pixi-live2d/assets/haru/sounds/tapBody_01.mp3"
+			"file": "static/pixi-live2d/assets/haru/motions/tapBody_01.mtn",
+			"sound": "static/pixi-live2d/assets/haru/sounds/tapBody_01.mp3"
 		}, {
-			"file": "pixi-live2d/assets/haru/motions/tapBody_02.mtn",
-			"sound": "pixi-live2d/assets/haru/sounds/tapBody_02.mp3"
+			"file": "static/pixi-live2d/assets/haru/motions/tapBody_02.mtn",
+			"sound": "static/pixi-live2d/assets/haru/sounds/tapBody_02.mp3"
 		}],
 		"pinch_in": [{
-			"file": "pixi-live2d/assets/haru/motions/pinchIn_00.mtn",
-			"sound": "pixi-live2d/assets/haru/sounds/pinchIn_00.mp3"
+			"file": "static/pixi-live2d/assets/haru/motions/pinchIn_00.mtn",
+			"sound": "static/pixi-live2d/assets/haru/sounds/pinchIn_00.mp3"
 		}],
 		"pinch_out": [{
-			"file": "pixi-live2d/assets/haru/motions/pinchOut_00.mtn",
-			"sound": "pixi-live2d/assets/haru/sounds/pinchOut_00.mp3"
+			"file": "static/pixi-live2d/assets/haru/motions/pinchOut_00.mtn",
+			"sound": "static/pixi-live2d/assets/haru/sounds/pinchOut_00.mp3"
 		}],
 		"shake": [{
-			"file": "pixi-live2d/assets/haru/motions/shake_00.mtn",
-			"sound": "pixi-live2d/assets/haru/sounds/shake_00.mp3",
+			"file": "static/pixi-live2d/assets/haru/motions/shake_00.mtn",
+			"sound": "static/pixi-live2d/assets/haru/sounds/shake_00.mp3",
 			"fade_in": 500
 		}],
 		"flick_head": [{
-			"file": "pixi-live2d/assets/haru/motions/flickHead_00.mtn",
-			"sound": "pixi-live2d/assets/haru/sounds/flickHead_00.mp3"
+			"file": "static/pixi-live2d/assets/haru/motions/flickHead_00.mtn",
+			"sound": "static/pixi-live2d/assets/haru/sounds/flickHead_00.mp3"
 		}]
 	}
 };
